ebd5e49558524a8f7b8c695c5b4d49286b299f62
Smoren <ofigate@gmail.com>
2022-03-06 20:31:36 +0300

    cast helper added

---
+++ src/CastHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Smoren\Helpers;
+
+/**
+ * Class CastHelper
+ * @package Smoren\Helpers
+ */
+class CastHelper
+{
+    /**
+     * Приводит объект к другому типу
+     * @param $sourceObject
+     * @param string $destinationClass
+     * @return mixed
+     */
+    public static function castObject($sourceObject, string $destinationClass)
+    {
+        return unserialize(sprintf(
+            'O:%d:"%s"%s',
+            strlen($destinationClass),
+            $destinationClass,
+            strstr(strstr(serialize($sourceObject), '"'), ':')
+        ));
+    }
+}
